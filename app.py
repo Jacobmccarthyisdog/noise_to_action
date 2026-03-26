9308393d303eaa6b4579c22a3da4d83918dafe71
Jacobmccarthyisdog <jacobmccarthy19@gmail.com>
2026-03-25 22:00:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ st.markdown(
         }
 
         .hero-title {
-            font-size: 2.6rem;
+            font-size: 3rem;
             line-height: 1.05;
             font-weight: 800;
             color: #F6FBFF;
