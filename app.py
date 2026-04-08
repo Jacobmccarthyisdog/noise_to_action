a5334e8767ab1776e8b8db86c77044784eb25838
Jacobmccarthyisdog <jacobmccarthy19@gmail.com>
2026-04-08 17:07:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ from charts import (
 st.set_page_config(
     page_title="Portfolio Dashboard",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="collapesed",
 )
 
 st.markdown(APP_CSS, unsafe_allow_html=True)
